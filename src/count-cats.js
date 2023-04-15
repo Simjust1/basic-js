a69840911a548235ed05bb8755806ae5523de2ba
Simjust1 <simjust@mail.ru>
2023-04-15 10:36:22 +0500

    feat: do count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,8 +14,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(array/* matrix */) {
+  //throw new NotImplementedError('Not implemented');
+
+
+  let number;
+  let array1 = [];
+
+    for (let i = 0; i < array.length; i++) {
+      for (let y = 0; y < array[i].length; y++) {
+      array1.push(array[i][y]);
+      }
+    }
+
+  for(let i = 0; i < array1.length; i++) {
+      number = array1.filter(e => {
+        if(e === '^^') {
+        return true;
+        }
+        else if(array1 === []) {
+          return 0;
+        }
+        return false;
+      }).length;
+
+
+       return number;
+  };
+
+    if(array1 === undefined || array1.length == 0) {
+          return number = 0;
+        }
+
+
   // remove line with error and write your code here
 }
 
--- src/dream-team.js
+++ src/dream-team.js
@@ -2,20 +2,31 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(array/* members */) {
+
+  if(array === null && array === false && array === undefined) {
+      return false;
+    }
+
+if (!Array.isArray(array)) {return false};
+
+  let array1 = array.filter(el => { if(typeof el === 'string') {return true; } return false; });
+  array1 = array1.map(el => el.trim().slice(0,1));
+  array1 = array1.map(el => el[0].toUpperCase());
+  array1 = array1.sort();
+  const str = array1.join('');
+  return str;
 }
 
 module.exports = {
